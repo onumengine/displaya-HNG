cd326094cacf8e69d3a014181abed8e2a600b893
onumdev <onumdev@gmail.com>
2021-08-17 13:56:45 +0100

    Add montserrat font files

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -6,8 +6,13 @@ class SplashScreen extends StatelessWidget {
       body: SafeArea(
         child: Stack(
           fit: StackFit.expand,
+          alignment: AlignmentDirectional.center,
           children: <Widget>[
             Image.asset('lib/assets/splash_img.png'),
+            Positioned(
+              bottom: MediaQuery.of(context).size.height / 10,
+              child: Text('Welcome'),
+            ),
           ],
         ),
       ),
