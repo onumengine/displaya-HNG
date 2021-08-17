8fb598aba720681152c971abe93e03fe633f793c
onumdev <onumdev@gmail.com>
2021-08-17 15:14:44 +0100

    turn the splash screen to a stateful widget

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,25 @@
+import 'dart:async';
+
+import 'package:displaya/profile_screen.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    var timer = Timer(Duration(milliseconds: 500), () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => ProfileScreen(),
+        ),
+      );
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
