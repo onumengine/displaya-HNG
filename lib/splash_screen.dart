779a111418695ba6f5855eddd7308d75d5fc85f0
onumdev <onumdev@gmail.com>
2021-08-17 14:33:34 +0100

    center the splash image

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -4,16 +4,11 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(
-          fit: StackFit.expand,
-          alignment: AlignmentDirectional.center,
-          children: <Widget>[
-            Image.asset('lib/assets/splash_img.png'),
-            Positioned(
-              bottom: MediaQuery.of(context).size.height / 10,
-              child: Text('Welcome'),
-            ),
-          ],
+        child: Center(
+          child: Image.asset(
+            'lib/assets/doormat.png',
+            width: 150,
+          ),
         ),
       ),
     );
