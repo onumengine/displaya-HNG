cf98b04411a30ae86296529b22c4e9005ca56ba4
onumdev <onumdev@gmail.com>
2021-08-18 02:18:50 +0100

    change the project directory structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:displaya/splash_screen.dart';
+import 'package:displaya/ui/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:displaya/profile_screen.dart';
+import 'package:displaya/ui/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
