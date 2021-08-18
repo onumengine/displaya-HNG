b1a88338f46983129aa14906c2d323273a3ef074
onumdev <onumdev@gmail.com>
2021-08-18 09:38:52 +0100

    wrap the list of profile details  in a FutureBuilder

--- lib/ui/screens/profile_screen.dart
+++ lib/ui/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreen extends StatefulWidget {
   _ProfileScreenState createState() => _ProfileScreenState();
@@ -9,12 +10,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
     var screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-      backgroundColor: Colors.purple.shade900,
+      backgroundColor: Colors.deepPurpleAccent,
       appBar: AppBar(
         automaticallyImplyLeading: true,
         elevation: 0,
         centerTitle: true,
-        backgroundColor: Colors.purple.shade900,
+        backgroundColor: Colors.deepPurpleAccent,
         title: Text('Profile'),
         actions: [
           IconButton(
@@ -24,115 +25,121 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ],
       ),
       body: SafeArea(
-        child: ListView(
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 65,
-                child: Image.asset('lib/assets/avatar.png'),
-              ),
-            ),
-            Container(
-              height: screenSize.height * 2 / 3,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40),
-                ),
-              ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 8),
-                      child: Text(
-                        'Username',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          height: 2,
-                          color: Colors.black26,
+        child: FutureBuilder<SharedPreferences>(
+          future: SharedPreferences.getInstance(),
+          builder: (context, snapshot) =>
+              (snapshot.connectionState == ConnectionState.done)
+                  ? ListView(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(vertical: 30),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            radius: 65,
+                            child: Image.asset('lib/assets/avatar.png'),
+                          ),
                         ),
-                      ),
-                    ),
-                    Text(
-                      'Your username goes here',
-                      style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Divider(),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 8),
-                      child: Text(
-                        'Email',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          height: 2,
-                          color: Colors.black26,
+                        Container(
+                          height: screenSize.height * 2 / 3,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(40),
+                              topRight: Radius.circular(40),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 50, horizontal: 30),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8),
+                                  child: Text(
+                                    'Username',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      height: 2,
+                                      color: Colors.black26,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  snapshot.data?.getString('username') ?? '...',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Divider(),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8),
+                                  child: Text(
+                                    'Email',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      height: 2,
+                                      color: Colors.black26,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  snapshot.data?.getString('email') ?? '...',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Divider(),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8),
+                                  child: Text(
+                                    'Phone',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      height: 2,
+                                      color: Colors.black26,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  snapshot.data?.getString('phone') ?? '...',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Divider(),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8),
+                                  child: Text(
+                                    'Zip code',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      height: 2,
+                                      color: Colors.black26,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  snapshot.data?.getString('zipCode') ?? '...',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    Text(
-                      'Your email goes here',
-                      style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Divider(),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 8),
-                      child: Text(
-                        'Phone',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          height: 2,
-                          color: Colors.black26,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      'Your phone number goes here',
-                      style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Divider(),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 8),
-                      child: Text(
-                        'Gender',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          height: 2,
-                          color: Colors.black26,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      'Your gender goes here',
-                      style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+                      ],
+                    )
+                  : CircularProgressIndicator(),
         ),
       ),
     );
