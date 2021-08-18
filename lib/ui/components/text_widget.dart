55f748efdcdf8c29d5c6d07725f40b6a2f3e1ad2
onumdev <onumdev@gmail.com>
2021-08-18 02:19:21 +0100

    require the hint text

---
+++ lib/ui/components/text_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class TextWidget extends StatelessWidget {
+  final String hintText;
+  final GlobalKey key;
+  final TextEditingController controller;
+  String enteredText;
+
+  TextWidget({
+    required this.key,
+    required this.hintText,
+    required this.controller,
+  }) : enteredText = controller.text;
+
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      decoration: BoxDecoration(
+        color: Colors.black12,
+        borderRadius: BorderRadius.all(
+          Radius.circular(16.0),
+        ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          SizedBox(width: 8.0),
+          Expanded(
+            child: TextField(
+              controller: this.controller,
+              textAlignVertical: TextAlignVertical.bottom,
+              decoration: InputDecoration.collapsed(
+                hintText: this.hintText,
+                hintStyle: TextStyle(
+                  color: Colors.black26,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: 8.0),
+        ],
+      ),
+    );
+  }
+}
